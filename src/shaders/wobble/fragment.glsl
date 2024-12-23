1af26f14cccc320228bc56d3768b200cc25fa9b2
humbertoragazzini <beto_vicio@hotmail.com>
2024-12-23 14:37:56 +0000

    fixed small issue in the simplexnooise

--- src/shaders/wobble/fragment.glsl
+++ src/shaders/wobble/fragment.glsl
@@ -1,14 +1,4 @@
-varying vec2 vUv;
-#include ../includes/simplexNoise4d.glsl
-
-
 void main(){
-  //wobble
-  float wobble = simplexNoise4d(
-        csm_Position,
-        0.0
-      );
-  csm_Position+=wobble * normal;
 
 }
 
--- src/shaders/wobble/vertex.glsl
+++ src/shaders/wobble/vertex.glsl
@@ -1,3 +1,13 @@
-varying vec2 vUv;
+#include ../includes/simplexNoise4d.glsl;
+
+
 void main(){
+  //wobble
+  float wobble = simplexNoise4d(
+        vec4(
+          csm_Position,
+          0.0
+        )
+      );
+  csm_Position += wobble * normal;
 }
