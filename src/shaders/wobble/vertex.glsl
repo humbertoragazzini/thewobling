2f07f44384905e34a866f33f1fc45603194dee16
humbertoragazzini <beto_vicio@hotmail.com>
2024-12-24 12:46:08 +0000

    fixed small error in the shader

--- src/shaders/wobble/vertex.glsl
+++ src/shaders/wobble/vertex.glsl
@@ -1,11 +1,19 @@
 attribute vec4 tangent;
 #include ../includes/simplexNoise4d.glsl;
 
+float getWobble(vec3 position){
+  return simplexNoise4d(
+        vec4(
+          position,
+          0.0
+        )
+      );
+}
 
 void main(){
 
   //tangent
-  vec3 biTangent = cross(nomral,tangent.xyz)
+  vec3 biTangent = cross(normal,tangent.xyz);
 
   //neighbors
   float shift = 0.01;  
@@ -13,11 +21,6 @@ void main(){
   vec3 positionB = csm_Position + biTangent.xyz  * shift;
   
   //wobble
-  float wobble = simplexNoise4d(
-        vec4(
-          csm_Position,
-          0.0
-        )
-      );
+  float wobble = getWobble(csm_Position);
   csm_Position += wobble * normal;
 }
